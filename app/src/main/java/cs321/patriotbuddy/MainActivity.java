cbfa219b786d54eb2b7ddb835eee7da4d76510c8
ojayyike <ojayyike@gmail.com>
2018-02-14 16:08:50 -0500

    init commint

--- app/src/main/java/cs321/patriotbuddy/MainActivity.java
+++ app/src/main/java/cs321/patriotbuddy/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //OJ push testw
+        //OJ push test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
