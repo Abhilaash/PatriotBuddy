0445b2e58dcfb5a9c1e0c00f098ff8ebc846b641
Zac Olsen <zolsen@masonlive.gmu.edu>
2018-03-28 21:18:07 -0400

    zacs test commit

--- app/src/main/java/cs321/patriotbuddy/MainActivity.java
+++ app/src/main/java/cs321/patriotbuddy/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 //added comment
+//zacs test commit
 public class MainActivity extends AppCompatActivity {
 
     @Override
